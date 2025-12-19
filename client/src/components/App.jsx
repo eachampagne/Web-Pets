3ed5df77d0b0dcd31e274b399934c41d3d172892
cfields05 <senken1218@gmail.com>
2025-12-19 15:54:56 -0600

    moved sign in to app and added state + effect hook for setting username to display on client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import DeviceView from './DeviceView';
+
 const App = () => {
+  const [ user, setUser ] = useState({ name: '' });
+
+  useEffect(() => {
+    axios.get('/user')
+      .then(({ data }) => {
+        setUser({ name: data });
+      })
+      .catch(err => {
+        console.error('Could not get user from client: ', err);
+      });
+  }, []);
+
   const handleLogout = function() {
-    axios.post('/logout', {});
+    axios.post('/logout', {})
+      .then(() => {
+        setUser({ name: '' });
+      })
+      .catch(err => {
+        console.error('Could not post from client: ', err);
+      });
   };
 
+  const { name } = user;
   return (
     <div>
       <h1>Rendering</h1>
+      <h2>{ name ? `Currently logged in as ${name}` : 'Please sign in!' }</h2>
       <button onClick={handleLogout}>Logout</button>
+      <div>
+        <h1>Sign in</h1>
+        <a className="button google" href="/login/federated/google">Sign in with Google</a>
+      </div>
+      <DeviceView />
     </div>
   );
 };
