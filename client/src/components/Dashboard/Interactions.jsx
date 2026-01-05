67c42293821d74494ae6a6335124d94846c0bd8f
cfields05 <senken1218@gmail.com>
2026-01-05 09:28:35 -0600

    documentation for interactions component

--- client/src/components/Dashboard/Interactions.jsx
+++ client/src/components/Dashboard/Interactions.jsx
@@ -2,6 +2,11 @@
 import React from 'react';
 import axios from 'axios';
 
+/**
+ * @module Interactions
+ * @description A component that displays and interacts with the pet's statuses.
+ * Allows the user to increase the pet's stats, which will decay each day.
+ */
 const Interactions = ({ pet, refreshPet, displayMessage }) => {
 
   const interactionTabStyles = [
@@ -10,8 +15,18 @@ const Interactions = ({ pet, refreshPet, displayMessage }) => {
     'p-[10px]'
   ];
 
+  /**
+   * Does the actual incrementing PATCH request to the server, sending required info and then refreshing the pet
+   * once it's finished. Uses a path parameter to determine which stat to update, and sends the amount to update by via request body.
+   * @name incrementStats
+   * @function
+   * @param {statName} statName - The stat increased when the incrementStats function fires its PATCH request. It's sent as a path parameter
+   * for the server to determine which status the user wants to increase.
+   * @param {amount} amount - This parameter determines how much the stat is increased by. The calculation for adding this amount to the
+   * status is handled on the server, but is essentially added directly to the server's version of the stat, to prevent client-server desync
+   * between what the client and the server thinks the pet's true stats are.
+   */
   const incrementStats = (statName, amount) => {
-    // amount = Math.min(amount + pet[statName], 100);
     axios.patch(`/interact/${statName}`, { amount })
       .then(refreshPet)
       .catch(err => {
@@ -19,6 +34,14 @@ const Interactions = ({ pet, refreshPet, displayMessage }) => {
       });
   };
 
+  /**
+   * Does the logic check for which request to send and if the player SHOULD be able to do an interaction.
+   * Calls the incrementStats function, passing in a corresponding stat and the amount to increment by.
+   * @name checkLogic
+   * @function
+   * @param {event} event - The event fired by clicking an interaction button. The event's target holds the data for
+   * which stat the button should increment.
+   */
   const checkLogic = ({ target: { value }}) => {
     switch (value) {
       case 'feed':
@@ -26,7 +49,7 @@ const Interactions = ({ pet, refreshPet, displayMessage }) => {
         incrementStats('hunger', 10);
         break;
       case 'play':
-        if (pet.mood > 50) {
+        if (pet.mood >= 50) {
           displayMessage(`${pet.name} is very playful!`);
           incrementStats('mood', 10);
         } else {
